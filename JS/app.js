6c5830a3b5f38b077615018b47e9fff4d25b8ef7
our sites <73804975+FelixArgeil@users.noreply.github.com>
2020-11-08 18:26:12 +0300

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -7,7 +7,7 @@ const linkE = document.querySelectorAll('.link__e');
 
 
 
-
+//Попап окна, Кривые
 linkQ.forEach(linkClickedQ);
 function linkClickedQ (item) {
 	item.addEventListener("click", function() {
@@ -32,7 +32,7 @@ function linkClickedE (item) {
 
 
 const animItems = document.querySelectorAll('._anim-items');	/*К классу   ._anim-items   добавляется класс   _active   */
-
+//Анимация при скролле
 if (animItems.length > 0) {
 	window.addEventListener('scroll', animOnScroll);
 	function animOnScroll() {
@@ -66,10 +66,36 @@ if (animItems.length > 0) {
 
 
 
-
+//menuBurger
 const menuIcon = document.querySelector('.hamburger__menu');
 const navBar = document.querySelector('.section__menu');
 
 menuIcon.addEventListener('click', () => {
 	navBar.classList.toggle("change");
 })
+
+
+//Плавный Скролл
+var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+    V = .180;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+        e.preventDefault(); //отменяем стандартное поведение
+        var w = window.pageYOffset,  // производим прокрутка прокрутка
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+            start = null;
+        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  // URL с хэшем
+            }
+        }
+    }, false);
+}
